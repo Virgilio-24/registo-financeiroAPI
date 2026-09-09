012dcf9fde62efd1607e25ca7cc1750b0f686d85
MADIGITAL\virgilio.jose <virgilio.jose@inovadigital.eu>
2026-09-09 14:52:06 +0100

    Add RequestLoggingMiddleware for logging incoming requests and responses

---
+++ RegistoFinanceiro.Api/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistoFinanceiro.Api.Middleware
+{
+    public class RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var startTime = DateTime.UtcNow;
+            logger.LogInformation("Incoming {Method} {Path}", context.Request.Method, context.Request.Path);
+
+            await next(context);
+
+            var elapsed = DateTime.UtcNow - startTime;
+            logger.LogInformation("Completed {Method} {Path} with {StatusCode} in {Elapsed}ms",
+                context.Request.Method, context.Request.Path, context.Response.StatusCode, elapsed.TotalMilliseconds);
+        }
+    }
+}
--- RegistoFinanceiro.Api/Program.cs
+++ RegistoFinanceiro.Api/Program.cs
@@ -1,11 +1,28 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using RegistoFinanceiro.Application.Configuration;
+using RegistoFinanceiro.Application.Services;
+using RegistoFinanceiro.Domain.Entities;
+using RegistoFinanceiro.Infrastructure.Email;
+using RegistoFinanceiro.Infrastructure.Identity;
 using RegistoFinanceiro.Infrastructure.Persistence;
-
+using RegistoFinanceiro.Api.Middleware; 
+using Scalar.AspNetCore;
+using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1.0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+}).AddMvc();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -15,16 +32,53 @@ builder.Services.AddDbContext<RegistoFinanceiroDbContext>(options =>
     options.UseNpgsql(connectionString)
            .UseSnakeCaseNamingConvention());
 
+builder.Services.Configure<JwtOptions>(
+    builder.Configuration.GetSection(JwtOptions.SectionName));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<RegistoFinanceiroDbContext>()
+    .AddDefaultTokenProviders();
+
+var jwtOptions = builder.Configuration
+    .GetSection(JwtOptions.SectionName)
+    .Get<JwtOptions>() ?? new JwtOptions();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtOptions.Issuer,
+        ValidAudience = jwtOptions.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SigningKey))
+    };
+});
+
+builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
+builder.Services.AddScoped<IEmailSender, LoggingEmailSender>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
+
+app.UseMiddleware<RequestLoggingMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
